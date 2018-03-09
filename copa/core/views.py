1963593745263ed19e0fbfb74d02c7343d45ee0d
elton jefferson <eltonjncorreia@gmail.com>
2018-03-09 19:06:42 -0300

    grupos

--- copa/core/views.py
+++ copa/core/views.py
@@ -6,14 +6,18 @@ from copa.usuario.views import create_users, empty_form
 
 def home(request):
     jogo = Jogo.objects.all()
-
     return render(request, 'core/index.html', {'jogos': jogo, 'grupo': grupo})
 
 
 def grupo(request):
-    grupo = Selecao.objects.filter(grupo__nome_grupo='A')
+    grupo_a = Selecao.objects.filter(grupo__nome_grupo='A').order_by('nome')
 
-    return render(request, 'core/grupos.html', {'grupo': grupo})
+    context = {
+        'grupo_a': grupo_a,
+
+    }
+
+    return render(request, 'core/grupos.html', context)
 
 
 
