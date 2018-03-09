85aaf2679df644aa606a929dc4233e29d0847f8b
elton jefferson <eltonjncorreia@gmail.com>
2018-03-09 18:32:39 -0300

    include de grupos

--- copa/core/urls.py
+++ copa/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from copa.core.views import home
+from copa.core.views import home, grupo
 
 app_name = 'core'
 urlpatterns = [
     path('', home, name='home'),
+    path('grupos/', grupo, name='grupo'),
 ]
