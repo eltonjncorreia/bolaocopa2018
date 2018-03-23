1e2203743fafd33b634603e9e81a3e7c1c39feeb
elton jefferson <eltonjncorreia@gmail.com>
2018-03-23 11:51:45 -0300

    apostas

--- copa/core/urls.py
+++ copa/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from copa.core.views import home, grupo
+from copa.core.views import home, grupo, aposta
 
 app_name = 'core'
 urlpatterns = [
     path('', home, name='home'),
     path('grupos/', grupo, name='grupo'),
+    path('<int:jogo_id>/aposta/', aposta, name='aposta'),
 ]
--- copa/core/views.py
+++ copa/core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, resolve_url as r
 
 from copa.core.models import Jogo, Grupo, Selecao
 
@@ -23,7 +24,24 @@ def grupo(request):
     return render(request, 'core/grupos.html', context)
 
 
+def aposta(request, jogo_id):
 
+    try:
+        jogo_id = get_object_or_404(Jogo, pk=jogo_id)
+    except Jogo.DoesNotExist:
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': jogo_id,
+            'error_message': "You didn't select a choice.",
+        })
+    # else:
+        # selected_choice.votes += 1
+        # selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+    # return HttpResponseRedirect(r('core:home'))
+    return render(request, 'core/apostas.html', {'id': jogo_id})
 
 
 
