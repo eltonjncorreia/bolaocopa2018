8932dc21c44a946cfad42470f037b39565ba35c5
elton jefferson <eltonjncorreia@gmail.com>
2018-03-23 22:16:41 -0300

    nova html mais elegante

--- copa/core/views.py
+++ copa/core/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render, get_object_or_404, resolve_url as r
 from copa.core.models import Jogo, Grupo, Selecao, Aposta
 # from datetime import date
 
+
 def home(request):
+
     # hoje = date.today().day
     # jogo = Jogo.objects.filter(horario__day=hoje).order_by('horario')
     jogo = Jogo.objects.all().order_by('horario')
-    return render(request, 'core/index.html', {'jogos': jogo})
+    return render(request, 'core/home.html', {'jogos': jogo})
 
 
 def grupo(request):
@@ -37,8 +39,8 @@ def aposta(request, jogo_id):
                               placar_1=placa1,
                               placar_2=placa2,
                               preco=preco)
-
-        return HttpResponseRedirect(r('core:home'))
+        mensagem = 'Deu'
+        return HttpResponseRedirect(r('core:home', mensagem))
     else:
         return HttpResponseRedirect(r('core:minha_aposta'))
 
