bb51ad25157c44f6de756c4ea60e6dee2b9dfe71
elton jefferson <eltonjncorreia@gmail.com>
2018-03-29 09:49:07 -0300

    media s3

--- copa/settings.py
+++ copa/settings.py
@@ -142,10 +142,7 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = '{}.storage_backends.MediaStorage'.format('https://bolaocopa2018.herokuapp.com/')  # <-- here is where we reference it
-
-
-# Extra places for collectstatic to find static files.
+DEFAULT_FILE_STORAGE = '{}.storage_backends.MediaStorage'.format('https://bolaocopa2018.herokuapp.com/')
 
 
 # static files
