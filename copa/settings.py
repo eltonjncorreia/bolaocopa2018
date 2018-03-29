f0fab8e8ab5dab9bde81f7783bfbe1e19ead1036
elton jefferson <eltonjncorreia@gmail.com>
2018-03-29 09:59:49 -0300

    media s3

--- copa/settings.py
+++ copa/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
+    os.path.join(BASE_DIR, '{}/static').format('https://bolaocopa2018.herokuapp.com/'),
 ]
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
