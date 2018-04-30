2e93c4fb09b1968d414b29338af5efee25aa046f
elton jefferson <eltonjncorreia@gmail.com>
2018-04-30 12:47:16 -0300

    aws

--- copa/urls.py
+++ copa/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bolao/', include('copa.core.urls', namespace='core')),
     path('', include('copa.usuario.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
